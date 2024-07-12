d2568022c4394311170b525b438b67705d3d421b
Anupratee <anupratee.bharadwaj@gmail.com>
2024-07-12 11:08:09 +0530

    added default images

--- backend/connectora/utils.py
+++ backend/connectora/utils.py
@@ -4,4 +4,7 @@ CLOUD_NAME = "daxlzqjke"
 API_KEY = "423476535432448"
 API_SECRET = "uqVIryJIZB5gB9uBUm3Qhy4LJ1o"
 
-DEFAULT_PLAYLIST_IMAGE = "https://res.cloudinary.com/daxlzqjke/image/upload/v1707745689/cld-sample-2.jpg"
+DEFAULT_SPONSOR_IMAGE = "https://asset.cloudinary.com/daxlzqjke/9701f4b015c6c77da93853c41637e490"
+DEFAULT_INFLUENCER_IMAGE = "https://asset.cloudinary.com/daxlzqjke/33367be5396809a2507a886857d18e46"
+ADMIN_IMAGE = "https://asset.cloudinary.com/daxlzqjke/d42e0c19cc6bd13e7613da16521303df"
+
